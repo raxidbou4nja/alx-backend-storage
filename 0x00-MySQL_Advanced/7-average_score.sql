4e183d7a798ee8b485d3fc69db3f8132b5f48753
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2024-04-25 18:41:04 +0100

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,29 +1,14 @@
 -- Create procedure ComputeAverageScoreForUser
-CREATE PROCEDURE ComputeAverageScoreForUser (
-    @user_id INT
-)
-AS
+DELIMITER **
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    -- Declare variable to hold average score
-    DECLARE @average_score DECIMAL(10, 2);
+	UPDATE users
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
 
-    -- Calculate average score
-    SELECT @average_score = AVG(score)
-    FROM corrections
-    WHERE user_id = @user_id;
+END **
 
-    -- Check if the user already has an entry in the average_scores table
-    IF EXISTS (SELECT 1 FROM average_scores WHERE user_id = @user_id)
-    BEGIN
-        -- Update the existing entry
-        UPDATE average_scores
-        SET average_score = @average_score
-        WHERE user_id = @user_id;
-    END
-    ELSE
-    BEGIN
-        -- Insert a new entry for the user
-        INSERT INTO average_scores (user_id, average_score)
-        VALUES (@user_id, @average_score);
-    END;
-END;
+DELIMITER ;
