ee32cde79356a91e7b94178663de9b0c0797dfce
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2024-04-25 18:50:53 +0100

    Create 10-div.sql

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- Script SQL
+DELIMITER //
+
+DROP FUNCTION IF EXISTS SafeDiv;
+
+CREATE FUNCTION SafeDiv (
+    a INT,
+    b INT
+)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    RETURN (IF (b = 0, 0, a / b));
+END //
+
+DELIMITER ;
