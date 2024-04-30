67dce01ee670b55bcececdb1153f0e4a537b2c24
root <rachid.boughnja@gmail.com>
2024-04-30 16:22:38 -0700

    add new fn

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,8 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
+from functools import wraps
 """
     cache
 """
@@ -13,20 +14,29 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @staticmethod
+    def count_calls(method: Callable) -> Callable:
+        @wraps(method)
+        def wrapper(self, *args, **kwargs):
+            key = method.__qualname__
+            self._redis.incr(key)
+            return method(self, *args, **kwargs)
+        return wrapper
+
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str,
-            fn: Callable = None) -> Union[str, bytes, int, None]:
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, None]:
         value = self._redis.get(key)
         if value is None:
             return None
         if fn is not None:
             return fn(value)
         return value
- 
+
     def get_str(self, key: str) -> Union[str, bytes, int, None]:
         return self.get(key, fn=lambda d: d.decode("utf-8"))
 
