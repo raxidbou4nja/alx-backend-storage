44b967cbcf8f1e4f366a99cd3e61b8c36e6a897a
root <rachid.boughnja@gmail.com>
2024-04-30 16:05:24 -0700

    add new file

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import redis
+import uuid
+from typing import Union
+"""
+    cache
+"""
+
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
