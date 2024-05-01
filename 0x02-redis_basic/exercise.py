cf2fc34f5237ca44c36c85578315b7ee1f4cf7fb
root <rachid.boughnja@gmail.com>
2024-04-30 17:07:53 -0700

    add new file

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import redis
 import uuid
-from typing import Union, Callable
+import redis
 from functools import wraps
+from typing import Any, Callable, Union
 """
     cache
 """
@@ -12,57 +12,60 @@ from functools import wraps
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
-        self._redis.flushdb()
+        self._redis.flushdb(True)
 
-    @staticmethod
-    def count_calls(method: Callable) -> Callable:
-        @wraps(method)
-        def wrapper(self, *args, **kwargs):
-            key = method.__qualname__
-            self._redis.incr(key)
-            return method(self, *args, **kwargs)
-        return wrapper
-
-    @staticmethod
-    def call_history(method: Callable) -> Callable:
-        @wraps(method)
-        def wrapper(self, *args, **kwargs):
-            input_key = method.__qualname__ + ":inputs"
-            output_key = method.__qualname__ + ":outputs"
-            self._redis.rpush(input_key, str(args))
-            output = method(self, *args, **kwargs)
-            self._redis.rpush(output_key, output)
-            return output
-        return wrapper
-
-    @count_calls
-    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        data_key = str(uuid.uuid4())
+        self._redis.set(data_key, data)
+        return data_key
 
     def get(self, key: str,
-            fn: Callable = None) -> Union[str, bytes, int, None]:
-        value = self._redis.get(key)
-        if value is None:
-            return None
-        if fn is not None:
-            return fn(value)
-        return value
+            fn: Callable = None) -> Union[str, bytes, int, float]:
+        data = self._redis.get(key)
+        return fn(data) if fn else data
 
-    def get_str(self, key: str) -> Union[str, bytes, int, None]:
-        return self.get(key, fn=lambda d: d.decode("utf-8"))
+    def get_str(self, key: str) -> str:
+        return self.get(key, lambda x: x.decode('utf-8'))
 
-    def get_int(self, key: str) -> Union[str, bytes, int, None]:
-        return self.get(key, fn=int)
+    def get_int(self, key: str) -> int:
+        return self.get(key, lambda x: int(x))
 
-    def replay(self, method: Callable) -> None:
-        method_name = method.__qualname__
-        input_key = method_name + ":inputs"
-        output_key = method_name + ":outputs"
-        inputs = self._redis.lrange(input_key, 0, -1)
-        outputs = self._redis.lrange(output_key, 0, -1)
-        print(f"{method_name} was called {len(inputs)} times:")
-        for inp, out in zip(inputs, outputs):
-            print(f"{method_name}(*{inp}) -> {out}")
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return invoker
+
+def call_history(method: Callable) -> Callable:
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        in_key = '{}:inputs'.format(method.__qualname__)
+        out_key = '{}:outputs'.format(method.__qualname__)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(in_key, str(args))
+        output = method(self, *args, **kwargs)
+        if isinstance(self._redis, redis.Redis):
+            self._redis.rpush(out_key, output)
+        return output
+    return invoker
+
+def replay(fn: Callable) -> None:
+    if not hasattr(fn, '__self__')
+    or not isinstance(fn.__self__._redis, redis.Redis):
+        return
+    redis_store = fn.__self__._redis
+    fxn_name = fn.__qualname__
+    in_key = '{}:inputs'.format(fxn_name)
+    out_key = '{}:outputs'.format(fxn_name)
+    fxn_call_count = int(redis_store.get(fxn_name) or 0)
+    print('{} was called {} times:'.format(fxn_name, fxn_call_count))
+    fxn_inputs = redis_store.lrange(in_key, 0, -1)
+    fxn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
+        print('{}(*{}) -> {}'.format(
+            fxn_name,
+            fxn_input.decode("utf-8"),
+            fxn_output,
+        ))
